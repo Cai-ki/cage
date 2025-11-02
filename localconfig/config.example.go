c18eeaed53121132853aeb208d404fd758b14b4b
Caiki <2875028086@qq.com>
2025-11-02 20:24:18 +0800

    Update config.example.go

--- localconfig/config.example.go
+++ localconfig/config.example.go
@@ -1,11 +1,12 @@
 package localconfig
 
-import "os"
-
-var (
-	AnyAPIToken = "your-api-token"
-)
-
 func init() {
-	os.Setenv("your-key", "your-value")
+	// os.Setenv("EMBEDDING_MODEL", "")
+	// os.Setenv("EMBEDDING_URL", "")
+	// os.Setenv("EMBEDDING_API_KEY", "")
+
+	// os.Setenv("SMTP_SERVICE", "")
+	// os.Setenv("SMTP_EMAIL", "")
+	// os.Setenv("SMTP_PASSWORD", "")
+	// os.Setenv("SMTP_NAME", "")
 }
